015c5e7919bebf382af5da5ba1e96b19665adc3c
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2009-09-12 10:59:44 +0200

    Better default value for the file parameter.

--- src/main/java/nu/mulli/clojureshell/ClojureSwankMojo.java
+++ src/main/java/nu/mulli/clojureshell/ClojureSwankMojo.java
@@ -1,6 +1,7 @@
 package nu.mulli.clojureshell;
 
 import clojure.lang.Repl;
+import java.io.*;
 
 /**
  * Goal which starts a Clojure REPL and starts running Swank.
@@ -24,13 +25,16 @@ public class ClojureSwankMojo extends AbstractClassloaderMojo {
 	protected String protocolVersion;
 
 	/**
-	 * @parameter expression="${clojure.swank.file}" default-value="/tmp/swank"
+	 * @parameter expression="${clojure.swank.file}" 
 	 *             
 	 */
 	protected File file;
 
 	@Override
 	protected void doExecute() throws Exception {
-		Repl.main(new String[] { "@loader.clj", "--", file, Integer.toString(port), protocolVersion });
+		if (file == null) {
+		    file = new File(new File(System.getProperty("java.io.tmpdir")), "swank");
+		}
+		Repl.main(new String[] { "@loader.clj", "--", file.getAbsolutePath(), Integer.toString(port), protocolVersion });
 	}
 }
